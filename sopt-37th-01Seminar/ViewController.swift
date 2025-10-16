7546dfe6e0186914d3b03c4172f870e33c4e4fa7
JIN <jin724596@gmail.com>
2025-10-16 12:01:53 +0900

    첫번째 과제추가

--- sopt-37th-01Seminar/ViewController.swift
+++ sopt-37th-01Seminar/ViewController.swift
@@ -1,19 +1,17 @@
-//
-//  ViewController.swift
-//  sopt-37th-01Seminar
-//
-//  Created by JIN on 10/11/25.
-//
-
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+extension UIView {
 
 
 }
+
+import UIKit
+ 
+class ViewController: UIViewController {
+    
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+}
+
 
